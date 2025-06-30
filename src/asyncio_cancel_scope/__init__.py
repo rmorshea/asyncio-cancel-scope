5d2bd4af699fee9ee6e310a8e508c21a049c9cab
Ryan Morshead <ryan.morshead@gmail.com>
2025-06-30 02:16:05 -0600

    fix lint

--- src/asyncio_cancel_scope/__init__.py
+++ src/asyncio_cancel_scope/__init__.py
@@ -46,16 +46,13 @@ class _CancelScope:
         """Cancel the associated task group - same interface as Task.cancel()."""
         self._cancelled_scope = True
         return self._task.cancel(msg)
-        
 
     async def __aenter__(self) -> tuple[TaskGroup, CancelCallback]:
         self._did_enter = Event()
         self._will_exit = Event()
         self._did_exit = Event()
 
-        self._task = create_task(
-            _wrapper(self.task_group, self._did_enter, self._will_exit)
-        )
+        self._task = create_task(_wrapper(self.task_group, self._did_enter, self._will_exit))
 
         did_enter_task = create_task(self._did_enter.wait())
 
