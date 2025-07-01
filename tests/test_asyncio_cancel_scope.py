ab0bf67a24f70c3b576b8ff4247ca151d758bb73
Ryan Morshead <ryan.morshead@gmail.com>
2025-06-30 23:58:56 -0600

    actually implement scopes

--- tests/test_asyncio_cancel_scope.py
+++ tests/test_asyncio_cancel_scope.py
@@ -3,6 +3,7 @@ import asyncio
 
 import pytest
 
+from asyncio_cancel_scope import cancel_group
 from asyncio_cancel_scope import cancel_scope
 
 
@@ -26,15 +27,15 @@ class FakeError(Exception):
 
 async def test_cancel_scope():
     never = asyncio.Event()
-    async with cancel_scope(asyncio.TaskGroup()) as (tg, cancel):
+    async with cancel_scope(asyncio.TaskGroup()) as tg:
         tg.create_task(never.wait())
-        cancel()
+        cancel_group(tg)
 
 
 async def test_cancel_scope_with_exception():
     never = asyncio.Event()
     with pytest.raises(ExceptionGroup):
-        async with cancel_scope(asyncio.TaskGroup()) as (tg, _):
+        async with cancel_scope(asyncio.TaskGroup()) as tg:
             tg.create_task(never.wait())
             raise FakeError
 
@@ -54,7 +55,7 @@ async def test_outer_cancel_causes_task_group_to_cancel():
             raise
 
     async def wrapper():
-        async with cancel_scope(asyncio.TaskGroup()) as (tg, _):
+        async with cancel_scope(asyncio.TaskGroup()) as tg:
             tg.create_task(wait_forever())
 
     task = asyncio.create_task(wrapper())
@@ -78,7 +79,7 @@ async def test_outer_error_causes_inner_cancel():
             raise
 
     with pytest.raises(ExceptionGroup):
-        async with cancel_scope(asyncio.TaskGroup()) as (tg, _):
+        async with cancel_scope(asyncio.TaskGroup()) as tg:
             tg.create_task(wait_forever())
             await is_waiting.wait()
             raise FakeError
@@ -108,5 +109,53 @@ async def test_internal_error_is_propagated():
         raise FakeError
 
     with pytest.raises(ExceptionGroup):
-        async with cancel_scope(asyncio.TaskGroup()) as (tg, _):
+        async with cancel_scope(asyncio.TaskGroup()) as tg:
             tg.create_task(will_raise())
+
+
+async def test_outer_task_group_is_cancelled_by_inner_due_to_shared_scope():
+    forever = asyncio.Event()
+
+    async def inner(outer_tg: asyncio.TaskGroup):
+        async with cancel_scope(asyncio.TaskGroup(), outer_tg) as inner_tg:
+            inner_tg.create_task(forever.wait())
+            # This will cancel the outer task group
+            cancel_group(inner_tg)
+
+    async with asyncio.timeout(1):
+        async with cancel_scope(asyncio.TaskGroup()) as tg:
+            tg.create_task(forever.wait())
+            tg.create_task(inner(tg))
+
+
+async def test_outer_task_group_not_cancelled_by_inner_if_no_shared_scope():
+    forever = asyncio.Event()
+
+    async def inner():
+        async with cancel_scope(asyncio.TaskGroup()) as inner_tg:
+            inner_tg.create_task(forever.wait())
+            # This will not cancel the outer task group
+            cancel_group(inner_tg)
+
+    async with cancel_scope(asyncio.TaskGroup()) as tg:
+        inner_task = tg.create_task(inner())
+        # If the outer task group were cancelled this would raise CancelledError
+        await inner_task
+
+
+async def test_create_task_on_cancelled_group_does_not_run():
+
+    async def raise_error():
+        raise RuntimeError("This task should not run")
+
+    async with cancel_scope(asyncio.TaskGroup()) as tg:
+        cancel_group(tg)
+        await asyncio.sleep(0)
+        # This coroutine should immediately be closed without running
+        tg.create_task(raise_error())
+
+
+def test_cannot_cancel_task_group_without_cancel_scope():
+    tg = asyncio.TaskGroup()
+    with pytest.raises(RuntimeError, match=r"cancel_scope"):
+        cancel_group(tg)
